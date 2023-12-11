c2ab18b810cf25dc88ea9cb2c51784df9743b36a
hikaroeduardo <hikaroeduardobandeira@gmail.com>
2023-12-11 20:16:59 -0300

    feat: add requisicao em API com criptomoedas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,60 @@
+import { useEffect, useState } from "react";
 import styles from "./home.module.css";
 
 import { Link } from "react-router-dom";
 
 import { BiSearch } from "react-icons/bi";
 
+interface CoinProps {
+  name: string;
+  delta_24h: string;
+  price: string;
+  symbol: string;
+  volume_24h: string;
+  market_cap: string;
+  formatedPrice: string;
+  formatedMarket: string;
+}
+
+interface DataProps {
+  coins: CoinProps[];
+}
+
 export function Home() {
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+
+  useEffect(() => {
+    function getData() {
+      fetch(
+        "https://sujeitoprogramador.com/api-cripto/?key=72c205de2350a35c&pref=BRL"
+      )
+        .then((response) => response.json())
+        .then((data: DataProps) => {
+          const coinsData = data.coins.slice(0, 15);
+
+          const price = Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+          })
+
+          const formatResult = coinsData.map((item) => {
+            const formated = {
+              ...item,
+              formatedPrice: price.format(Number(item?.price)),
+              formatedMarket: price.format(Number(item?.market_cap))
+            }
+
+            return formated;
+          })
+
+          setCoins(formatResult);
+        })
+        .catch((err) => console.log(err));
+    }
+
+    getData();
+  }, []);
+
   return (
     <main className={styles.container}>
       <form className={styles.form}>
@@ -33,9 +83,13 @@ export function Home() {
               </Link>
             </td>
 
-            <td className={styles.tdLabel} data-label="Mercado">R$ 19293</td>
+            <td className={styles.tdLabel} data-label="Mercado">
+              R$ 19293
+            </td>
 
-            <td className={styles.tdLabel} data-label="Preço">R$ 40.456</td>
+            <td className={styles.tdLabel} data-label="Preço">
+              R$ 40.456
+            </td>
 
             <td className={styles.tdProfit} data-label="Volume">
               <span>-5.3</span>
